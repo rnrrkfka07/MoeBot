0656c6d6b8dede3318e88872edd94fc148510e0d
G4ram <garam4266211@gmail.com>
2025-06-08 22:36:58 +0900

    temp

--- obj/Debug/net9.0/moeBot.AssemblyInfo.cs
+++ obj/Debug/net9.0/moeBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moeBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e94ec61324292ecf8b23e5b3b4fa79adcaf1c5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("moeBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moeBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
