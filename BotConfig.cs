f882a49a2782440fbffb7d586a8c21d0f475f4fd
G4ram <65856313+rnrrkfka07@users.noreply.github.com>
2025-06-08 22:39:40 +0900

    Create BotConfig.cs

---
+++ BotConfig.cs
@@ -0,0 +1,6 @@
+public static class BotConfig
+{
+   public const string DiscordToken = " "; // 여기 님들 디스코드 봇 토큰 적으셈
+   public const string GeminiApiKey = " "; // 여기는 제미나이 api 키 적으시면 됩니다
+   public const string VoicevoxApiBaseUrl = "http://localhost:50021"; // 이건 VoiceVox 기본 경로니까 건드리지 않으셔도 됌.
+};
